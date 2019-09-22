c3cef699da5bc83cdf10791e7af585cb7f8bfe0c
Nils Bergmann <Nils1794@gmail.com>
2019-09-22 21:34:59 +0200

    Fix RunEvaluation

--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/RunEvaluation.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/RunEvaluation.java
@@ -1,20 +1,19 @@
 package bergmann.masterarbeit.generationtarget;
 
-import bergmann.masterarbeit.generationtarget.dataaccess.State;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 import bergmann.masterarbeit.generationtarget.dataaccess.DataController;
 import bergmann.masterarbeit.generationtarget.dataaccess.DatabaseWrapper;
-import bergmann.masterarbeit.generationtarget.expressions.*;
-import bergmann.masterarbeit.generationtarget.interfaces.Expression;
+import bergmann.masterarbeit.generationtarget.dataaccess.State;
 import bergmann.masterarbeit.generationtarget.utils.Assertion;
+import bergmann.masterarbeit.generationtarget.expressions.*;
 import bergmann.masterarbeit.generationtarget.utils.UserVariable;
+import bergmann.masterarbeit.generationtarget.interfaces.Expression;
 
 class RunEvaluation {
     public static void main(String args[]) {
-        DataController dataControl = new DataController();
+        DataController dataControl = new DataController(false);
         dataControl.connectToDatabase("test.db");
         ArrayList<Assertion> assertions = new ArrayList<Assertion>();
 
