455ffc8790fb97a93438e3268b2b308b8a829e2c
Nils Bergmann <Nils1794@gmail.com>
2019-09-22 21:38:32 +0200

    Add example for CustomExpression

---
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/example/CustomExpression.java
@@ -0,0 +1,7 @@
+package bergmann.masterarbeit.generationtarget.example;
+
+import bergmann.masterarbeit.generationtarget.interfaces.*;
+
+public class CustomExpression extends BinaryExpression<Boolean, Double, Boolean>{
+
+}
