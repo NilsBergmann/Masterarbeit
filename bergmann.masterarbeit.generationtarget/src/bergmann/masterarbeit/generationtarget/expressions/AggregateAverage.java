d821283e2d6870b65ed4d3b0c063ac55976730fc
Nils Bergmann <Nils1794@gmail.com>
2019-10-05 15:46:07 +0200

    Add Aggregate function implementations

--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/AggregateAverage.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/AggregateAverage.java
@@ -1,5 +1,7 @@
 package bergmann.masterarbeit.generationtarget.expressions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.jscience.physics.amount.Amount;
@@ -8,6 +10,7 @@ import bergmann.masterarbeit.generationtarget.dataaccess.DataController;
 import bergmann.masterarbeit.generationtarget.dataaccess.State;
 import bergmann.masterarbeit.generationtarget.interfaces.Expression;
 import bergmann.masterarbeit.generationtarget.interfaces.UnaryExpression;
+import bergmann.masterarbeit.generationtarget.utils.AbsoluteTimeInterval;
 import bergmann.masterarbeit.generationtarget.utils.RelativeTimeInterval;
 
 public class AggregateAverage extends UnaryExpression<Amount, Amount> {
@@ -20,7 +23,45 @@ public class AggregateAverage extends UnaryExpression<Amount, Amount> {
 
     @Override
     public Optional<Amount> evaluate(State state, DataController dataSource) {
-        // TODO: Implement this
-        return Optional.empty();
+        boolean realTime = dataSource.isRealTime();
+        AbsoluteTimeInterval relevantTime = this.interval.addInstant(state.timestamp);
+
+        // Check if data is complete
+        if (realTime && !dataSource.intervalIsInRange(relevantTime)) {
+            return Optional.empty();
+        }
+
+        // Get states
+        List<State> relevantStates = dataSource.getStatesInInterval(relevantTime);
+        if (relevantStates.size() == 0)
+            return Optional.empty();
+
+        // Evaluate expr for every state
+        List<Optional<Amount>> results = new ArrayList<Optional<Amount>>();
+        for (State current : relevantStates) {
+            results.add(expr.evaluate(current, dataSource));
+        }
+
+        // Add them all up
+        try {
+            Amount sum = null;
+            for (Optional<Amount> current : results) {
+                if (!current.isPresent())
+                    // Value missing? return UNKNOWN
+                    return Optional.empty();
+                if (sum == null)
+                    sum = current.get();
+                else
+                    sum.plus(current.get());
+            }
+
+            // Divide by n
+            Amount result = sum.divide(results.size());
+
+            return Optional.of(result);
+        } catch (javax.measure.converter.ConversionException e) {
+            System.err.println("Mismatched units in average calculation, returning UNKNOWN");
+            return Optional.empty();
+        }
     }
 }
--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/AggregateMaximum.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/AggregateMaximum.java
@@ -1,5 +1,7 @@
 package bergmann.masterarbeit.generationtarget.expressions;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.jscience.physics.amount.Amount;
@@ -8,6 +10,7 @@ import bergmann.masterarbeit.generationtarget.dataaccess.DataController;
 import bergmann.masterarbeit.generationtarget.dataaccess.State;
 import bergmann.masterarbeit.generationtarget.interfaces.Expression;
 import bergmann.masterarbeit.generationtarget.interfaces.UnaryExpression;
+import bergmann.masterarbeit.generationtarget.utils.AbsoluteTimeInterval;
 import bergmann.masterarbeit.generationtarget.utils.RelativeTimeInterval;
 
 public class AggregateMaximum extends UnaryExpression<Amount, Amount> {
@@ -20,7 +23,40 @@ public class AggregateMaximum extends UnaryExpression<Amount, Amount> {
 
     @Override
     public Optional<Amount> evaluate(State state, DataController dataSource) {
-        // TODO: Implement this
-        return Optional.empty();
+        boolean realTime = dataSource.isRealTime();
+        AbsoluteTimeInterval relevantTime = this.interval.addInstant(state.timestamp);
+
+        // Check if data is complete
+        if (realTime && !dataSource.intervalIsInRange(relevantTime)) {
+            return Optional.empty();
+        }
+
+        // Get states
+        List<State> relevantStates = dataSource.getStatesInInterval(relevantTime);
+        if (relevantStates.size() == 0)
+            return Optional.empty();
+
+        // Evaluate expr for every state
+        List<Optional<Amount>> results = new ArrayList<Optional<Amount>>();
+        for (State current : relevantStates) {
+            results.add(expr.evaluate(current, dataSource));
+        }
+        try {
+            // Find maximum
+            Amount maximum = null;
+            for (Optional<Amount> current : results) {
+                if (!current.isPresent())
+                    // Value missing? return UNKNOWN
+                    return Optional.empty();
+                if (maximum == null)
+                    maximum = current.get();
+                else if (current.get().isGreaterThan(maximum))
+                    maximum = current.get();
+            }
+            return Optional.of(maximum);
+        } catch (javax.measure.converter.ConversionException e) {
+            System.err.println("Mismatched units in average calculation, returning UNKNOWN");
+            return Optional.empty();
+        }
     }
 }
