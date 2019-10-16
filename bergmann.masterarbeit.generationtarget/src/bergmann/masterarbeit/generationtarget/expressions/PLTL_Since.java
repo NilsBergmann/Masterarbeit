8eb43c391090ad0d8cb1f2f420f88823659a85df
Nils Bergmann <Nils1794@gmail.com>
2019-10-16 19:16:14 +0200

    Implement Since and Trigger

--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/PLTL_Since.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/PLTL_Since.java
@@ -1,11 +1,15 @@
 package bergmann.masterarbeit.generationtarget.expressions;
 
+import java.util.List;
 import java.util.Optional;
+
+import javax.swing.plaf.FontUIResource;
 
 import bergmann.masterarbeit.generationtarget.dataaccess.DataController;
 import bergmann.masterarbeit.generationtarget.dataaccess.State;
 import bergmann.masterarbeit.generationtarget.interfaces.BinaryExpression;
 import bergmann.masterarbeit.generationtarget.interfaces.Expression;
+import bergmann.masterarbeit.generationtarget.utils.AbsoluteTimeInterval;
 import bergmann.masterarbeit.generationtarget.utils.RelativeTimeInterval;
 
 public class PLTL_Since extends BinaryExpression<Boolean, Boolean, Boolean> {
@@ -22,11 +26,72 @@ public class PLTL_Since extends BinaryExpression<Boolean, Boolean, Boolean> {
 
     @Override
     public Optional<Boolean> evaluate(State state) {
-        // TODO: Implement this
-        return Optional.empty();
+        // The S(for “Since”) operator is the temporal dual of U(until), so that (x S y)
+        // is true iff y holds somewhere in the past and x is true from then up to now.
+        List<State> relevantStates = null;
+        boolean hasInterval = this.interval != null;
+        boolean realTime = state.dataController.isRealTime();
+
+        // Get relevant states
+        if (hasInterval) {
+            AbsoluteTimeInterval relevantTime = this.interval.addInstant(state.timestamp);
+            relevantStates = state.dataController.getStatesInInterval(relevantTime);
+        } else {
+            relevantStates = state.dataController.getAllStatesBefore(state);
+            relevantStates.add(state);
+        }
+
+        // Iterate over states
+        // - Find first occurance of right==true
+        // - After that: left must be true
+        Optional<Boolean> foundRight = Optional.of(false);
+        for (State currentState : relevantStates) {
+            if (foundRight.isPresent()) {
+                if (foundRight.get() == false) {
+                    Optional<Boolean> rightEval = this.right.evaluate(currentState);
+                    // Right wasnt found yet
+                    if (!rightEval.isPresent()) {
+                        // Right is unknown in this state
+                        foundRight = Optional.empty();
+                    } else if (rightEval.isPresent() && rightEval.get() == true) {
+                        Optional<Boolean> leftEval = this.left.evaluate(currentState);
+                        // Right is true in this state
+                        foundRight = Optional.of(true);
+                        // Check if left is true as well
+                        if (!leftEval.isPresent())
+                            return Optional.empty();
+                        else if (leftEval.get() == false)
+                            return Optional.of(false);
+                    } else if (rightEval.isPresent() && rightEval.get() == false) {
+                        // Right is false in this state
+                    }
+                } else {
+                    // Right was found, from now on left has to be true
+                    Optional<Boolean> leftEval = this.left.evaluate(currentState);
+                    if (!leftEval.isPresent())
+                        return Optional.empty();
+                    else if (leftEval.get() == false)
+                        return Optional.of(false);
+                }
+            } else {
+                // Right was unknown some time:
+                // From now on left must be true all the time, otherwise return unknown
+                Optional<Boolean> leftEval = this.left.evaluate(currentState);
+                if (!leftEval.isPresent() || leftEval.get() == false)
+                    return Optional.empty();
+            }
+        }
+
+        // No conflict found
+        // - Check if right has been found
+        // --- 1) If it was: return true
+        // --- 2) If it wasnt : return false (?)
+        // --- 3) If its unknown: return unknown, unless 2) is actually supposed to
+        // return true
+        return foundRight;
     }
-    
+
     public String toString() {
-    	return "S"+"("+left+","+right+")";
+        return "S" + "(" + left + "," + right + ")";
     }
 }
--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/PLTL_Trigger.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/expressions/PLTL_Trigger.java
@@ -10,6 +10,7 @@ import bergmann.masterarbeit.generationtarget.utils.RelativeTimeInterval;
 
 public class PLTL_Trigger extends BinaryExpression<Boolean, Boolean, Boolean> {
     RelativeTimeInterval interval;
+    Expression<Boolean> helper;
 
     public PLTL_Trigger(Expression<Boolean> left, Expression<Boolean> right) {
         this(left, right, null);
@@ -18,14 +19,18 @@ public class PLTL_Trigger extends BinaryExpression<Boolean, Boolean, Boolean> {
     public PLTL_Trigger(Expression<Boolean> left, Expression<Boolean> right, RelativeTimeInterval interval) {
         super(left, right);
         this.interval = interval;
+        // x T y = not ((not x) S (not ψ)) [Bounded Verification of Past LTL]
+        Expression<Boolean> notLeft = new BoolNegation(left);
+        Expression<Boolean> notRight = new BoolNegation(right);
+        helper = new BoolNegation(new PLTL_Since(notLeft, notRight, interval));
     }
 
     @Override
     public Optional<Boolean> evaluate(State state) {
-        // TODO: Implement this
-        return Optional.empty();
+        return helper.evaluate(state);
     }
+
     public String toString() {
-    	return "T"+"("+left+","+right+")";
+        return "T" + "(" + left + "," + right + ")";
     }
 }
