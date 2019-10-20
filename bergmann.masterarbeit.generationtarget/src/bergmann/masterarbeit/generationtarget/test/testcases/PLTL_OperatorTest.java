0a6462f649b2fe1fcc91ecb976392575a64b1576
Nils Bergmann <Nils1794@gmail.com>
2019-10-20 19:34:08 +0200

    Fix PLTL test file

--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/test/testcases/PLTL_OperatorTest.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/test/testcases/PLTL_OperatorTest.java
@@ -2,25 +2,14 @@ package bergmann.masterarbeit.generationtarget.test.testcases;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Instant;
-import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import bergmann.masterarbeit.generationtarget.dataaccess.DataController;
-import bergmann.masterarbeit.generationtarget.dataaccess.State;
-import bergmann.masterarbeit.generationtarget.expressions.And;
-import bergmann.masterarbeit.generationtarget.expressions.BoolDatabaseAccess;
-import bergmann.masterarbeit.generationtarget.expressions.BoolNegation;
-import bergmann.masterarbeit.generationtarget.expressions.Implication;
-import bergmann.masterarbeit.generationtarget.expressions.LTL_Finally;
-import bergmann.masterarbeit.generationtarget.expressions.LTL_Global;
-import bergmann.masterarbeit.generationtarget.expressions.LTL_Next;
 import bergmann.masterarbeit.generationtarget.interfaces.Expression;
 
-class PLTLOperatorsTest {
+class PLTL_OperatorTest {
 	static DataController ctrl;
 	static Expression a, b, expected;
 	
