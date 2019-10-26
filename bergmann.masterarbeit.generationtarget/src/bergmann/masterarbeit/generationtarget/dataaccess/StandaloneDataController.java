d5fddec2135415615a6a4a2efbcdf7c85930b5ff
Nils Bergmann <Nils1794@gmail.com>
2019-10-26 16:50:59 +0200

    FINALIZE Real time evaluation mode

--- bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/dataaccess/StandaloneDataController.java
+++ bergmann.masterarbeit.generationtarget/src/bergmann/masterarbeit/generationtarget/dataaccess/StandaloneDataController.java
@@ -3,277 +3,221 @@ package bergmann.masterarbeit.generationtarget.dataaccess;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
-import java.util.SortedSet;
 
 import javax.measure.unit.Unit;
 
-import bergmann.masterarbeit.generationtarget.utils.AbsoluteTimeInterval;
-import bergmann.masterarbeit.generationtarget.utils.Assertion;
 import bergmann.masterarbeit.generationtarget.utils.MonitorDeclaration;
 import bergmann.masterarbeit.generationtarget.utils.UIUtils;
-import bergmann.masterarbeit.generationtarget.utils.UserVariable;
 
 public class StandaloneDataController {
-    private DatabaseWrapper dbWrapper;
-    public StateListHandler stateHandler;
-    private boolean isRealTime = false;
+	private DatabaseWrapper dbWrapper;
+	public StateListHandler stateHandler;
+	private boolean isRealTime = false;
 
-    public void setRealTime(boolean isRealTime) {
+	public void setRealTime(boolean isRealTime) {
 		this.isRealTime = isRealTime;
 	}
 
 	public StandaloneDataController(boolean isRealTime) {
-    	stateHandler = new StateListHandler(isRealTime);
-        this.isRealTime = isRealTime;
-        this.dbWrapper = new DatabaseWrapper();
-    }
+		stateHandler = new StateListHandler(isRealTime);
+		this.isRealTime = isRealTime;
+		this.dbWrapper = new DatabaseWrapper();
+	}
 
-    public void connectToDatabase(String path) {
-        if (dbWrapper.isConnected()) {
-            dbWrapper.disconnect();
-        }
-        dbWrapper.connect(path);
-    }
+	public void connectToDatabase(String path) {
+		if (dbWrapper.isConnected()) {
+			dbWrapper.disconnect();
+		}
+		dbWrapper.connect(path);
+	}
 
-    public List<State> updateStates() {
-    	List<State> readStates = new ArrayList<State>();
-    	if(this.stateHandler.isEmpty())
-    		readStates = this.dbWrapper.getStates();
-    	else
-    		readStates = this.dbWrapper.getStatesAfter(this.stateHandler.getLatestState().timestamp);
-        for (State state : readStates) {
-			if(!stateHandler.contains(state)) {
-				stateHandler.add(state);				
+	public List<State> updateStates() {
+		List<State> readStates = new ArrayList<State>();
+		if (this.stateHandler.isEmpty())
+			readStates = this.dbWrapper.getStates();
+		else
+			readStates = this.dbWrapper.getStatesAfter(this.stateHandler.getLatestState().timestamp);
+		for (State state : readStates) {
+			if (!stateHandler.contains(state)) {
+				stateHandler.add(state);
 			}
 		}
-        return readStates;
-    }
+		return readStates;
+	}
 
-    public void selectTable(String tablename) {
-        if (this.dbWrapper != null && this.dbWrapper.isConnected()) {
-            try {
-                this.dbWrapper.setTable(tablename);
-                this.stateHandler = new StateListHandler(isRealTime);
-            } catch (IllegalArgumentException e) {
-                throw e;
-            }
-        }
-    }
+	public void selectTable(String tablename) {
+		if (this.isConnectedToDB()) {
+			try {
+				this.dbWrapper.setTable(tablename);
+				this.stateHandler = new StateListHandler(isRealTime);
+			} catch (IllegalArgumentException e) {
+				throw e;
+			}
+		} else {
+			System.err.println("No database connection");
+		}
+	}
 
-    public List<String> getTables() {
-        if (this.dbWrapper != null && this.dbWrapper.isConnected()) {
-            return this.dbWrapper.getTables();
-        }
-        return new ArrayList<String>();
-    }
+	public List<String> getTables() {
+		if (this.isConnectedToDB()) {
+			return this.dbWrapper.getTables();
+		}
+		System.err.println("No database connection");
+		return new ArrayList<String>();
+	}
 
-    public boolean isConnectedToDB() {
-        if (this.dbWrapper == null)
-            return false;
-        else
-            return this.dbWrapper.isConnected();
-    }
+	public boolean isConnectedToDB() {
+		return this.dbWrapper != null && this.dbWrapper.isConnected();
+	}
 
-    public boolean isRealTime() {
-        return this.isRealTime;
-    }
+	public boolean isRealTime() {
+		return this.isRealTime;
+	}
 
-    public void registerNumberDBColumn(String name, Unit unit) {
-    	System.out.println("Registered required DB Number: " + name + "["+unit+"]");
-        this.dbWrapper.registerNumberColumn(name, unit);
-    }
+	public void registerNumberDBColumn(String name, Unit unit) {
+		System.out.println("Registered required DB Number: " + name + "[" + unit + "]");
+		this.dbWrapper.registerNumberColumn(name, unit);
+	}
 
-    public void registerStringDBColumn(String name) {
-    	System.out.println("Registered required DB String: " + name );
-        this.dbWrapper.registerStringColumn(name);
-    }
+	public void registerStringDBColumn(String name) {
+		System.out.println("Registered required DB String: " + name);
+		this.dbWrapper.registerStringColumn(name);
+	}
 
-    public void registerBooleanDBColumn(String name) {
-    	System.out.println("Registered required DB Boolean: " + name );
-        this.dbWrapper.registerBooleanColumn(name);
-    }
-    
-    public void registerRequiredData(MonitorDeclaration monitors) {
-    	for (String id : monitors.getRequiredDataStrings()) {
+	public void registerBooleanDBColumn(String name) {
+		System.out.println("Registered required DB Boolean: " + name);
+		this.dbWrapper.registerBooleanColumn(name);
+	}
+
+	public void registerRequiredData(MonitorDeclaration monitors) {
+		for (String id : monitors.getRequiredDataStrings()) {
 			this.registerStringDBColumn(id);
 		}
-    	for (String id : monitors.getRequiredDataBooleans()) {
+		for (String id : monitors.getRequiredDataBooleans()) {
 			this.registerBooleanDBColumn(id);
 		}
-    	for (Map.Entry<String, Unit> entry : monitors.getRequiredDataNumbers().entrySet()) {
+		for (Map.Entry<String, Unit> entry : monitors.getRequiredDataNumbers().entrySet()) {
 			String id = entry.getKey();
 			Unit unit = entry.getValue();
 			this.registerNumberDBColumn(id, unit);
 		}
-    }
+	}
 
-    public void runEvaluation(MonitorDeclaration monitors, String tableName) {
-        if (this.isRealTime) {
-            this.runRealtimeEvaluation(monitors, tableName);
-        } else {
-            this.runNonRealtimeEvaluation(monitors, tableName);
-        }
-    }
+	public void runEvaluation(MonitorDeclaration monitors, String tableName) {
+		List<String> storedDBValues = new ArrayList<String>();
+		storedDBValues.addAll(monitors.getRequiredDataBooleans());
+		storedDBValues.addAll(monitors.getRequiredDataStrings());
+		storedDBValues.addAll(monitors.getRequiredDataNumbers().keySet());
+		List<String> storedUserVars = new ArrayList<String>(monitors.getDeclaredUserVariableNames());
+		List<String> storedAssertions = new ArrayList<String>(monitors.getDeclaredAssertionNames());
 
-    private void runRealtimeEvaluation(MonitorDeclaration monitors, String tableName) {
-        File folder = UIUtils.folderSelection();
-        String path = folder.toString() + "/" + monitors.getName() + "/";
-        File dir = new File(path);
-    	dir.mkdirs();
-    	File everything = new File(path +tableName+ ".csv");
-    	File onlyAssertions = new File(path + tableName+ "_OnlyAssertions.csv");
-    	
-    	this.registerRequiredData(monitors); 
-        this.dbWrapper.setTable(tableName);
-
-        try {
-        	while(true) {
-        		Thread.sleep(10);
-        		// Get states
-        		List<State> read = this.updateStates();
-        		// Evaluate States
-        		if(read != null && !read.isEmpty()) {
-        			System.out.println("Found " + read.size() + " new states");
-        			for (State state : read) {
-        				System.out.println("Evaluating: " + state);
-        				Map<String, Optional> result = monitors.evaluateAllAt(state);
-        			}
-        			System.out.println("Evaluation complete! Waiting for new states");
-        		} else {
-        		}
-        	}
-        } catch (InterruptedException e) {
-        	System.err.println("Loop interrupted. " + e);
-        }
+		// Select files and folders
+		File folder = UIUtils.folderSelection();
+		String path = folder.toString() + "/" + monitors.getName() + "/";
+		File dir = new File(path);
+		dir.mkdirs();
+		// stuff for console output
+		long timestamp = System.currentTimeMillis();
+		int stateCounter = 0;
+		this.registerRequiredData(monitors);
+		this.dbWrapper.setTable(tableName);
+		try {
+			FileWriter everythingFW = new FileWriter(new File(path + tableName + ".csv"));
+			FileWriter onlyAssertionsFW = new FileWriter(new File(path + tableName + "_OnlyAssertions.csv"));
+			// Write csv headers
+			this.writeHeader(everythingFW, storedDBValues, storedUserVars, storedAssertions);
+			this.writeHeader(onlyAssertionsFW, null, null, storedAssertions);
+			do {
+				// Try getting states
+				List<State> read = this.updateStates();
+				// Evaluate States if they exist
+				if (read != null && !read.isEmpty()) {
+					timestamp = System.currentTimeMillis();
+					System.out.println("Found " + read.size() + " states");
+					for (State state : read) {
+						stateCounter++;
+						System.out.println("Evaluating State #" + stateCounter + ": " + state);
+						Map<String, Optional> result = monitors.evaluateAllAt(state);
+						this.writeCSVRow(everythingFW, state, storedDBValues, storedUserVars, storedAssertions);
+						this.writeCSVRow(onlyAssertionsFW, state, null, null, storedAssertions);
+					}
+					System.out.println("Evaluation complete!");
+					everythingFW.flush();
+					onlyAssertionsFW.flush();
+				} else if(isRealTime) {
+					long delta = System.currentTimeMillis() - timestamp;
+					System.out.print("\rWaiting for new states in " + tableName + " (current amount: "+stateCounter+"). time since last update: " + delta + "ms");
+					Thread.sleep(5);
+				}
+			} while (isRealTime);
+		} catch (Exception e) {
+			System.err.println("Interrupted! " + e);
+		}
 		
 	}
 
-	private void runNonRealtimeEvaluation(MonitorDeclaration monitors, String tableName) {
-        File folder = UIUtils.folderSelection();
-    	this.registerRequiredData(monitors); 
-        this.dbWrapper.setTable(tableName);
-        // Get states
-        this.updateStates();
-        // Evaluate States
-        System.out.println("Running evaluations...\n");
-       
-        for (State state : this.stateHandler.getAllStates()) {
-        	System.out.println("Evaluating: " + state);
-        	Map<String, Optional> result = monitors.evaluateAllAt(state);
-        }
-        System.out.println("Evaluation complete!");
-        if(folder != null && folder.isDirectory())
-        	writeToCSV(folder.toString(), tableName, this.stateHandler.getAllStates(), monitors);
-    }
-	
-	private void writeHeader(File file, List<String> headers) {
+	private void writeHeader(FileWriter fw, List<String> selectedDBValues, List<String> selectedUserVariables,
+			List<String> selectedAssertions) {
 		try {
-			FileWriter fw = new FileWriter(file);
-			String header = "Timestamp;";
-			for (String string : headers) {
-				header += string + ";";
+			String header = "Timestamp; ";
+			if (selectedDBValues != null) {
+				for (String string : selectedDBValues) {
+					header += string + "; ";
+				}
 			}
+			if (selectedUserVariables != null) {
+				for (String string : selectedUserVariables) {
+					header += string + "; ";
+				}
+			}
+			if (selectedAssertions != null) {
+				for (String string : selectedAssertions) {
+					header += string + "; ";
+				}
+			}
+			header += "\n";
 			fw.write(header);
+			fw.flush();
 		} catch (Exception e) {
-			System.err.println("Writing header to " + file + "failed. " + e );
+			System.err.println("Writing header to " + fw + "failed. " + e);
 		}
 	}
-    
-    private void writeToCSV(String mainFolder, String tableName, List<State> states, MonitorDeclaration monitors) {
-    	
-    	String path = mainFolder + "/" + monitors.getName() + "/";
-    	System.out.println("Writing data to " + path);
-    	
-    	List<String> storedDBValues = new ArrayList<String>();
-    	storedDBValues.addAll(monitors.getRequiredDataBooleans());
-    	storedDBValues.addAll(monitors.getRequiredDataStrings());
-    	storedDBValues.addAll(monitors.getRequiredDataNumbers().keySet());
-    	List<String> storedUserVars = new ArrayList<String>(monitors.getDeclaredUserVariableNames());
-    	List<String> storedAssertions = new ArrayList<String>(monitors.getDeclaredAssertionNames());
-    	
-    	try {
-    	// Write everything.csv
-   		FileWriter fw = new FileWriter(everything);
-   		
-    	// Create header
-    	String header = "Timestamp; ";
-    	for (String string : storedDBValues) {
-    		header += string +";";
-    	}
-    	for (String string : storedUserVars) {
-    		header += string +";";
-    	}
-    	for (String string : storedAssertions) {
-    		header += string +";";
-    	}
-    	header+="\n";
-    	
-    	// Write state data
-    	fw.write(header);
-    	for (State state : states) {
-			String current = state.timestamp.toString()+ ";";
-			for (String key : storedDBValues) {
-				Optional data = state.getStoredDBValue(key);
-				String dataString = data.isPresent() ? data.get().toString() : " ";
-				current += dataString + ";";
+
+	private void writeCSVRow(FileWriter fw, State state, List<String> selectedDBValues, List<String> selectedUserVariables,
+			List<String> selectedAssertions) {
+		try {
+			String line = state.timestamp.toString() + ";";
+			if (selectedDBValues != null) {
+				for (String key : selectedDBValues) {
+					Optional data = state.getStoredDBValue(key);
+					String dataString = data.isPresent() ? data.get().toString() : " ";
+					line += dataString + "; ";
+				}
 			}
-			for (String key : storedUserVars) {
-				Optional data = state.getStoredUserVariableResult(key);
-				String dataString = data.isPresent() ? data.get().toString() : " ";
-				current += dataString + ";";
+			if (selectedUserVariables != null) {
+				for (String key : selectedUserVariables) {
+					Optional data = state.getStoredUserVariableResult(key);
+					String dataString = data.isPresent() ? data.get().toString() : " ";
+					line += dataString + "; ";
+				}
 			}
-			for (String key : storedAssertions) {
-				Optional data = state.getAssertionResult(key);
-				String dataString = data.isPresent() ? data.get().toString() : " ";
-				current += dataString + ";";
+			if (selectedAssertions != null) {
+				for (String key : selectedAssertions) {
+					Optional data = state.getAssertionResult(key);
+					String dataString = data.isPresent() ? data.get().toString() : "  ";
+					line += dataString + "; ";
+				}
 			}
-			current+="\n";
-			fw.write(current);
+			line += "\n";
+			fw.write(line);
+		} catch (IOException e) {
+			System.err.println("Could not write to file " + fw + ". " + e);
 		}
-    	fw.close();
-    	} catch (IOException e) {
-    		System.out.println("Writing results to csv failed. " + e);
-    	}
-    	
-    	try {
-    	// Write _OnlyAsssertions.csv
-   		FileWriter fw = new FileWriter(onlyAssertions);
-   		
-    	// Create header
-    	String header = "Timestamp; ";
-    	for (String string : storedAssertions) {
-    		header += string +";";
-    	}
-    	header+="\n";
-    	
-    	// Write state data
-    	fw.write(header);
-    	for (State state : states) {
-			String current = state.timestamp.toString()+ ";";
-			for (String key : storedAssertions) {
-				Optional data = state.getAssertionResult(key);
-				String dataString = data.isPresent() ? data.get().toString() : " ";
-				current += dataString + ";";
-			}
-			current+="\n";
-			fw.write(current);
-		}
-    	fw.close();
-    	} catch (IOException e) {
-    		System.err.println("Writing results to csv failed. " + e);
-    	}
-    	
-    }
-    
+
+	}
+
 }
